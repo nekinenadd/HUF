b8ae7ca123a747031320e10ddef00139c00d70e6
nekinenadd <nenad.djurdjevic2002@gmail.com>
2025-06-23 01:00:19 +0200

    HUF

---
+++ script.js
@@ -0,0 +1,6 @@
+function openForm(type) {
+  document.getElementById("cleaningType").value = type;
+  document.getElementById("contactModalLabel").textContent = "Contact Us - " + type + " Cleaning";
+  const contactModal = new bootstrap.Modal(document.getElementById('contactModal'));
+  contactModal.show();
+}
